a6d35e04eb6fbb7e6058b56cf69f15d2d4297389
loremus <loremus@proton.me>
2026-08-06 11:19:55 +0530

    unwrap, ifOk, ifOkAsync

--- result.ts
+++ result.ts
@@ -2,10 +2,40 @@ export type Result<T, E> =
   | { success: true; data: T }
   | { success: false; error: E };
 
-export function isOk<T, E>(data: T): Result<T, E> {
+export function ok<T, E>(data: T): Result<T, E> {
   return { success: true, data };
 }
 
-export function isError<T, E>(error: E): Result<T, E> {
+export function error<T, E>(error: E): Result<T, E> {
   return { success: false, error };
+}
+
+export function unwrap<T, E>(output: Result<T, E>, fun: (arg: E) => never): T {
+  if (!output.success) {
+    fun(output.error);
+  } else {
+    return output.data;
+  }
+}
+
+export function ifOk<T, E, R>(
+  output: Result<T, E>,
+  fun: (arg: T) => R,
+): Result<R, E> {
+  if (output.success) {
+    return ok(fun(output.data));
+  } else {
+    return error(output.error);
+  }
+}
+
+export async function ifOkAsync<T, E, R>(
+  output: Result<T, E>,
+  fun: (arg: T) => Promise<R>,
+): Promise<Result<R, E>> {
+  if (output.success) {
+    return ok(await fun(output.data));
+  } else {
+    return error(output.error);
+  }
 }
