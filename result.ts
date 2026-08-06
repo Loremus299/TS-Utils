eccb00b8f01c73dcb21c56344f2a6aaf88c374c7
loremus <loremus@proton.me>
2026-08-06 11:29:32 +0530

    shortening functions using ? operator

--- result.ts
+++ result.ts
@@ -11,53 +11,33 @@ export function error<T, E>(error: E): Result<T, E> {
 }
 
 export function unwrap<T, E>(output: Result<T, E>, fun: (arg: E) => never): T {
-  if (!output.success) {
-    fun(output.error);
-  } else {
-    return output.data;
-  }
+  return output.success ? output.data : fun(output.error);
 }
 
 export function ifOk<T, E, R>(
   output: Result<T, E>,
   fun: (arg: T) => R,
 ): Result<R, E> {
-  if (output.success) {
-    return ok(fun(output.data));
-  } else {
-    return error(output.error);
-  }
+  return output.success ? ok(fun(output.data)) : error(output.error);
 }
 
 export async function ifOkAsync<T, E, R>(
   output: Result<T, E>,
   fun: (arg: T) => Promise<R>,
 ): Promise<Result<R, E>> {
-  if (output.success) {
-    return ok(await fun(output.data));
-  } else {
-    return error(output.error);
-  }
+  return output.success ? ok(await fun(output.data)) : error(output.error);
 }
 
 export function ifError<T, E, F>(
   output: Result<T, E>,
   fun: (arg: E) => F,
 ): Result<T, F> {
-  if (!output.success) {
-    return error(fun(output.error));
-  } else {
-    return ok(output.data);
-  }
+  return output.success ? ok(output.data) : error(fun(output.error));
 }
 
 export async function ifErrorAsync<T, E, F>(
   output: Result<T, E>,
   fun: (arg: E) => Promise<F>,
 ): Promise<Result<T, F>> {
-  if (!output.success) {
-    return error(await fun(output.error));
-  } else {
-    return ok(output.data);
-  }
+  return output.success ? ok(output.data) : error(await fun(output.error));
 }
