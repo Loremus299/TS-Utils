0d93b93c6dd9a4b9a6dca0fe16641e3d1b4141e6
loremus <loremus@proton.me>
2026-08-09 01:23:24 +0530

    Result as a class

--- result.ts
+++ result.ts
@@ -1,93 +1,66 @@
-export type Result<T, E> =
+export type ResultType<T, E> =
   | { success: true; data: T }
   | { success: false; error: E };
 
-export function makeData<T, E>(data: T): Result<T, E> {
-  return { success: true, data };
-}
+export class Result<T, E> {
+  private readonly value: ResultType<T, E>;
 
-export function makeError<T, E>(error: E): Result<T, E> {
-  return { success: false, error };
-}
+  private constructor(value: ResultType<T, E>) {
+    this.value = value;
+  }
 
-export function unwrap<T, E>(output: Result<T, E>, fun: (arg: E) => never): T {
-  return output.success ? output.data : fun(output.error);
-}
+  public static ok<T, E>(data: T): Result<T, E> {
+    return new Result<T, E>({ success: true, data });
+  }
 
-export function mapData<T, E, R>(
-  output: Result<T, E>,
-  fun: (arg: T) => R,
-): Result<R, E> {
-  return output.success ? makeData(fun(output.data)) : makeError(output.error);
-}
+  public static error<T, E>(error: E): Result<T, E> {
+    return new Result<T, E>({ success: false, error: error });
+  }
 
-export async function mapDataAsync<T, E, R>(
-  output: Result<T, E>,
-  fun: (arg: T) => Promise<R>,
-): Promise<Result<R, E>> {
-  return output.success
-    ? makeData(await fun(output.data))
-    : makeError(output.error);
-}
+  public static async fallback<T, E, V>(
+    data: V,
+    defaultError: E,
+    funs: Array<(arg: V) => Promise<Result<T, E>>>,
+  ): Promise<Result<T, E>> {
+    let lastError: E = defaultError;
 
-export function mapError<T, E, F>(
-  output: Result<T, E>,
-  fun: (arg: E) => F,
-): Result<T, F> {
-  return output.success ? makeData(output.data) : makeError(fun(output.error));
-}
+    for (const fun of funs) {
+      const res: Result<T, E> = await fun(data);
+      if (res.value.success) {
+        return res;
+      }
 
-export async function mapErrorAsync<T, E, F>(
-  output: Result<T, E>,
-  fun: (arg: E) => Promise<F>,
-): Promise<Result<T, F>> {
-  return output.success
-    ? makeData(output.data)
-    : makeError(await fun(output.error));
-}
-
-export function fallback<T, E, V>(
-  data: V,
-  defaultError: E,
-  funs: Array<(arg: V) => Result<T, E>>,
-): Result<T, E> {
-  let lastError = defaultError;
-  for (const fun of funs) {
-    const res = fun(data);
-    if (res.success) {
-      return res;
+      lastError = res.value.error;
     }
 
-    lastError = res.error;
+    return Result.error<T, E>(lastError);
   }
-  return makeError<T, E>(lastError);
-}
 
-export async function fallbackAsync<T, E, V>(
-  data: V,
-  defaultError: E,
-  funs: Array<(arg: V) => Promise<Result<T, E>>>,
-): Promise<Result<T, E>> {
-  let lastError = defaultError;
-  for (const fun of funs) {
-    const res = await fun(data);
-    if (res.success) {
-      return res;
+  public static async tryCatch<T, V>(
+    data: V,
+    fun: (data: V) => Promise<T>,
+  ): Promise<Result<T, string>> {
+    try {
+      const res = await fun(data);
+      return Result.ok(res);
+    } catch (error) {
+      return Result.error(error as string);
     }
-
-    lastError = res.error;
   }
-  return makeError(lastError);
-}
 
-export async function tryCatch<T, V>(
-  data: V,
-  fun: (data: V) => Promise<T>,
-): Promise<Result<T, string>> {
-  try {
-    const res = await fun(data);
-    return makeData(res);
-  } catch (error) {
-    return makeError(error as string);
+  public unwrap(fun: (arg: E) => never): T {
+    return this.value.success ? this.value.data : fun(this.value.error);
+  }
+
+  public async mapOk<R>(fun: (arg: T) => Promise<R>): Promise<Result<R, E>> {
+    return this.value.success
+      ? Result.ok<R, E>(await fun(this.value.data))
+      : Result.error<R, E>(this.value.error);
+  }
+
+  public async mapError<F>(fun: (arg: E) => Promise<F>): Promise<Result<T, F>> {
+    return this.value.success
+      ? Result.ok<T, F>(this.value.data)
+      : Result.error<T, F>(await fun(this.value.error));
   }
 }
