5df0e59a06262f008b17087a83576e2548980177
loremus <loremus@proton.me>
2026-08-29 10:12:40 +0530

    base implementation

--- ops/postgres/drizzle/index.ts
+++ ops/postgres/drizzle/index.ts
@@ -1,34 +1,40 @@
-import type {
+import {
   and,
-  EmptyRelations,
-  InferInsertModel,
-  InferSelectModel,
-  or,
-  SQL,
+  eq,
+  getColumns,
+  type AnyColumn,
+  type EmptyRelations,
+  type InferInsertModel,
+  type InferSelectModel,
+  type or,
+  type SQL,
 } from "drizzle-orm";
 import type {
   AnyPgTable,
-  PgAsyncSelect,
   PgAsyncTransaction,
   PgQueryResultHKT,
 } from "drizzle-orm/pg-core";
-import type { QueryResult } from "pg";
 
 type DB = PgAsyncTransaction<PgQueryResultHKT, EmptyRelations>;
 type Condition<T> = (
   t: T,
 ) => SQL | ReturnType<typeof and> | ReturnType<typeof or>;
+type ExecutableQuery<TResult> = {
+  execute(): Promise<TResult>;
+  toSQL(): {
+    sql: string;
+    params: unknown[];
+  };
+};
 
-interface drizzleOpsInterface {
+interface DrizzleOpsInterface {
   insert: <T extends AnyPgTable>(
     table: T,
     data: InferInsertModel<T>,
     tx?: DB,
   ) => Promise<InferSelectModel<T>>;
 
-  readAll: <T extends PgAsyncSelect>(query: T) => Promise<QueryResult<T>>;
-
-  readUnique: <T extends PgAsyncSelect>(query: T) => Promise<QueryResult<T>>;
+  read: <TResult>(query: ExecutableQuery<TResult>) => Promise<TResult>;
 
   readTable: <T extends AnyPgTable>(
     table: T,
@@ -55,3 +61,88 @@ interface drizzleOpsInterface {
     tx?: DB,
   ) => Promise<InferSelectModel<T>>;
 }
+
+const drizzleOps: DrizzleOpsInterface = {
+  async insert(table, data, tx) {
+    const t = tx!;
+    const rows = (await t
+      .insert(table)
+      .values(data)
+      .returning()) as InferSelectModel<typeof table>[];
+
+    return rows[0] as InferSelectModel<typeof table>;
+  },
+
+  async read(query) {
+    return await query.execute();
+  },
+
+  async readTable(table, data, tx) {
+    const t = tx!;
+    const columns = getColumns(table);
+    const conditions: SQL[] = [];
+
+    for (const [key, value] of Object.entries(data)) {
+      if (value !== undefined && key in columns) {
+        console.log(key);
+        const column = columns[key as keyof typeof columns];
+        conditions.push(eq(column as AnyColumn, value as never));
+      }
+    }
+
+    return (await drizzleOps.read(
+      t
+        .select()
+        .from(table as AnyPgTable)
+        .where(and(...conditions)),
+    )) as InferSelectModel<typeof table>[];
+  },
+
+  async readTableUnique(table, data, tx) {
+    const t = tx!;
+    const columns = getColumns(table);
+    const conditions: SQL[] = [];
+
+    for (const [key, value] of Object.entries(data)) {
+      if (value !== undefined && key in columns) {
+        console.log(key);
+        const column = columns[key as keyof typeof columns];
+        conditions.push(eq(column as AnyColumn, value as never));
+      }
+    }
+
+    const rows = (await drizzleOps.read(
+      t
+        .select()
+        .from(table as AnyPgTable)
+        .where(and(...conditions)),
+    )) as InferSelectModel<typeof table>[];
+
+    return rows[0] as InferSelectModel<typeof table>;
+  },
+
+  async update(table, data, condition, tx) {
+    const t = tx!;
+
+    const rows = (await t
+      .update(table)
+      .set(data)
+      .where(condition(table))
+      .returning()) as InferSelectModel<typeof table>[];
+
+    return rows[0] as InferSelectModel<typeof table>;
+  },
+
+  async delete(table, condition, tx) {
+    const t = tx!;
+
+    const rows = (await t
+      .delete(table)
+      .where(condition(table))
+      .returning()) as InferSelectModel<typeof table>[];
+
+    return rows[0] as InferSelectModel<typeof table>;
+  },
+};
+
+export default drizzleOps;
